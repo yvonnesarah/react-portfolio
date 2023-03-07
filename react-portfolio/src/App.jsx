585526eb149fe5aa102ae37084ee84d267943927
Yvonne Adedeji <yvonne.sarah.adedeji@hotmail.com>
2023-03-07 13:27:05 +0000

    Added code for App.jsx

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+export default function App() {
+  return (
+    <Router basename="/">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
