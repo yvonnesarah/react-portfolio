8dccf9383a16bf0634eff2b71448d66437c466b2
Yvonne Adedeji <yvonne.sarah.adedeji@hotmail.com>
2023-03-07 19:38:43 +0000

    Updated projects.jsx - added function for project cards for the portfolio

---
+++ react-portfolio/src/pages/Projects.jsx
@@ -0,0 +1,30 @@
+import Layout from "../components/Layout";
+import ProjectCards from "../components/ProjectCards";
+
+import projects from "../data/portfolio.json";
+
+export default function Projects() {
+  console.log(projects);
+  return (
+    <Layout>
+      <div className="container-fluid">
+        <h1 className = "mb-4">Projects</h1>
+        <div class="row">
+          {projects.map((project) => {
+            return (
+              <div key={project.id} class="col-md-4 d-flex justify-content-center mb-5">
+                <ProjectCards
+                  deployed={project.deployed}
+                  repo={project.repo}
+                  tech={project.tech}
+                  name={project.name}
+                  img={project.img}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </Layout>
+  );
+}
