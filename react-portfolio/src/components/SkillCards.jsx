e11b56dc044eb26d55dd069a75ac24eff6c8cb5a
Yvonne Adedeji <yvonne.sarah.adedeji@hotmail.com>
2023-03-08 17:42:37 +0000

    Created skillcards.jsx - cards for skills

---
+++ react-portfolio/src/components/SkillCards.jsx
@@ -0,0 +1,17 @@
+import styles from "../styles/SkillCards.module.css";
+
+export default function SkillCard(props) {
+  return (
+    <div className={`card ${styles.cardCustom}`} style={{ width: "15rem" }}>
+      <img
+        src={props.img}
+        className="card-img-top"
+        height="80"
+        alt={props.img}
+      />
+      <div className="card-body">
+        <center><h6 className="card-title text-center text-light">{props.skills}</h6></center>
+      </div>
+    </div>
+  );
+}
